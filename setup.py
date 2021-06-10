5a9d3aaf986b15c20992999ef1249b128edd2083
Mario Bielert <mario.bielert@tu-dresden.de>
2021-06-10 11:30:49 +0200

    Adds black as dev dependency

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "examples": ["aiomonitor", "click", "click-log", "click-completion"],
         "tests": test_deps,
         "docs": ["sphinx", "sphinx-autodoc-typehints", "sphinxcontrib-trio"],
-        "dev": ["mypy", "isort"],
+        "dev": ["mypy", "isort", "black"],
     },
     project_urls={
         "Documentation": "https://aiocouch.readthedocs.io/en/stable",
